3ea4cad95a4f49c0b141a1589317bd29cca2baa5
BuildTools <unconfigured@null.spigotmc.org>
2018-04-07 10:35:04 -0700

    Test

--- src/me/noodles/gui/inv/ClickEvents.java
+++ src/me/noodles/gui/inv/ClickEvents.java
@@ -11,7 +11,6 @@ import me.noodles.gui.main.Main;
 public class ClickEvents implements Listener {
 
 	
-	
     @EventHandler
     public void onClick(InventoryClickEvent e) {
         if (e.getInventory().getName().equals(InvNames.Main)) {
